659fccb4c910e4a0eb1fddcccad696fc33fb3e8e
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-11-09 13:47:40 +0100

    plan page work and more detailview work

--- js/fetchData.js
+++ js/fetchData.js
@@ -81,17 +81,46 @@ export let showDetailedView = (id) => {
     <h2 class="attraction-title">${_selectedItem.Name}</h2>
     <h3 class="hashtag">#visitdenmark</h3>
   </div>
-  <h3 class="description-title">${_selectedItem}</h3>
-  <i class="favorite-icon"></i>
-  <p class="attraction-description"></p>
-  <button></button>
-  <button></button>
-  <h3></h3>
-  <i class="geo-pin"></i><p class="adress"></p>
-  <div id="map" class="map"></div>
   </article>
+  <div class="container">
+  <h3 class="description-title">${_selectedItem.Name}</h3>
+  <span class="addFav" onclick="addToFav()">
+  <svg width="29" height="26" viewBox="0 0 29 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M27.1977 4.10928L27.198 4.11003C28.68 7.12605 28.1562 10.6235 25.1947 14.6712L25.1929 14.6737C22.8518 17.8945 19.4704 21.1394 14.5043 24.9765C9.53302 21.1331 6.1571 17.8599 3.8131 14.6701C0.84326 10.6228 0.319768 7.12569 1.80156 4.11003L1.80193 4.10928C2.79668 2.08049 5.78423 0.273695 9.41196 1.30672C11.14 1.80301 12.6465 2.86656 13.686 4.32225L14.4998 5.4618L15.3136 4.32225C16.3533 2.86629 17.8601 1.80261 19.5886 1.30644L19.5912 1.3057C23.2036 0.258274 26.2011 2.07681 27.1977 4.10928Z" fill="white" stroke="#DF0921" stroke-width="2"/>
+</svg>
+</span>
+  </div>
+  <p class="attraction-description">${_selectedItem.Descriptions[0].Text}</p>
+  <div class="container">
+  <button class="detail-button see-more-btn" onclick="expandText()">Read more</button>
+  <button class="detail-button others-say-btn">See what others say</button>
+  </div>
+  <h3></h3>
+  <i class="geo-pin"></i><p class="address"></p>
+  <div id="map" class="map"></div>
+  
 `;
   navigateTo("#/detailView");
+};
+
+export let expandText = () => {
+  let attraction = document.querySelector(".attraction-description");
+  let button = document.querySelector(".see-more-btn");
+  if (attraction.classList.contains("open")) {
+    attraction.classList.remove("open");
+    button.innerHTML = "READ MORE";
+  } else {
+    attraction.classList.add("open");
+    button.innerHTML = "READ LESS";
+  }
+};
+
+export let addToFav = () => {
+  let span = document.querySelector(".addFav > svg > path");
+  /**
+   * Change color of heart icon when saved
+   * Add item to global array
+   */
 };
 
 /**
--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 "use strict";
 import "./spa.js";
 import "./fetchData.js";
-import { showDetailedView } from "./fetchData.js";
-import { search } from "./fetchData.js";
+
+import { expandText, search, showDetailedView, addToFav } from "./fetchData.js";
 window.showDetailedView = (id) => showDetailedView(id);
 window.search = (value) => search(value);
+window.expandText = () => expandText();
+window.addToFav = () => addToFav();
 
 /**
  * Hide tabbar on landing page
  */
 let hideTabbar = () => {
-    let home_section = document.querySelector("#home");
-    let tabbar = document.querySelector(".tabbar");
-    let header = document.querySelector("header");
-    if (home_section.style.display === "block") {
-        tabbar.style.display = "none";
-        header.style.display = "none";
-    }
+  let home_section = document.querySelector("#home");
+  let tabbar = document.querySelector(".tabbar");
+  let header = document.querySelector("header");
+  if (home_section.style.display === "block") {
+    tabbar.style.display = "none";
+    header.style.display = "none";
+  }
 };
 
 hideTabbar();
@@ -25,17 +27,17 @@ hideTabbar();
  * Animate city cards and logo on load
  */
 let load_home_page = () => {
-    let logo = document.querySelector(".logo-load");
-    let cards = document.querySelectorAll(".city-link");
-    let title = document.querySelector(".city-section-title");
-    window.addEventListener("load", () => {
-        logo.style.top = "0";
-        logo.style.width = "200px";
-        title.style.left = "18.75px";
-        for (const city of cards) {
-            city.style.left = "18.75px";
-        }
-    });
+  let logo = document.querySelector(".logo-load");
+  let cards = document.querySelectorAll(".city-link");
+  let title = document.querySelector(".city-section-title");
+  window.addEventListener("load", () => {
+    logo.style.top = "0";
+    logo.style.width = "200px";
+    title.style.left = "18.75px";
+    for (const city of cards) {
+      city.style.left = "18.75px";
+    }
+  });
 };
 
 load_home_page();
